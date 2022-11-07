f4b960b6ca112a0c7452c297371bdbd1b2163c0b
Dan1elH3ctor <danielhector080@gmail.com>
2022-11-07 16:50:17 +0200

    commit

---
+++ js/code.js
@@ -0,0 +1,43 @@
+// // write to a document
+// document.write("Hello World");
+// document.write("<br>My name is Daniel Hector");
+// document.write("<br>I am a Developer");
+// /*
+// You are Awesome*/
+// let username = `Daniel`;
+// let userage = `27`;
+// console.log(typeof userage)
+// console.log(typeof username);
+let userName = `Daniel`;
+let userAge = 27;
+let isOld = true;
+
+document.write(`name: ${userName}<br>Age: ${userage}<br>Old ${isOld}`)
+
+// document.write(userName);
+// document.write(`<br>`+ userAge);
+// document.write(`<br>`+ isOld);
+
+// making an array
+// let names = (`Aedirn`, `Daniel`, `Hector`)
+// console.log(typeof names)
+let salary = 6000;
+document.write(salary)
+salary = 50000;
+document.write(`<br>new salary : R${salary}`)
+// const val = .16;
+// val = .20;
+document.write(val)
+let person = {
+    name: `John`,
+    surname: `Bruce`,
+    age: 26
+};
+console.log(person);
+
+//function
+function greet() {
+    console.log(`hello`);
+}
+
+greet()
